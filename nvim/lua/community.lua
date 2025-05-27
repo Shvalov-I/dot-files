651db113c06aac82a843d8991aa97e9f2541b974
Shvalov-I <shvalov.i@gmail.com>
2025-05-27 23:00:18 +0700

    Added vscode to astrocommunity

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.recipes.vscode" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.typescript" },
