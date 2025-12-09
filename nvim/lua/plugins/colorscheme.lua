166de2490bff558e5ebd49c415dc04c3db44c5c8
Shvalov-I <shvalov.i@gmail.com>
2025-12-09 18:50:50 +0700

    change colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,30 @@
 return {
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
+			vim.opt.termguicolors = true
+
+			local catppuccin = require("catppuccin")
+
+			catppuccin.setup({
+				flavour = "mocha",
+				term_colors = true,
+				transparent_background = true,
 				styles = {
-					comments = { italic = false }, -- Disable italics in comments
+					comments = {},
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					native_lsp = { enabled = true },
+					telescope = true,
+					treesitter = true,
+					treesitter_context = true,
 				},
 			})
-
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,18 +2,8 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			sections = {
-				lualine_y = {
-					"location",
-				},
-				lualine_z = {
-					{
-						"datetime",
-						style = "%H:%M",
-					},
-				},
-			},
-		},
+		config = function()
+			require("lualine").setup({ options = { theme = "catppuccin" } })
+		end,
 	},
 }
